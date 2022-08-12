4a6fd89a6c606b895f254b45dc06b601ed80143f
DarinD06 <darindunlap06@gmail.com>
2022-08-12 16:19:13 -0500

    Finding the sum of digits of a positive integere number using recursion.

--- recursion.py
+++ recursion.py
@@ -1,5 +1,6 @@
 # Practicing recursive functions
 
+
 # Using factorials
 # The recursive case for factorial is n! = n * (n-1)!
 def factorial(n):
@@ -11,7 +12,7 @@ def factorial(n):
         return n * factorial(n - 1)  # Recursive state
 
 
-print(factorial(3))
+print("Factorial", factorial(3))
 
 
 # Fibonacci sequence
@@ -22,3 +23,16 @@ def fibonacci(n):
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)  # Recursive state
+
+print("Fibonacci:", fibonacci(5))
+
+
+# Finding sum of digits of a positive integer using recursion
+def sumOfDigits(n):
+    assert 0 <= n == int(n), 'The number must be a positive integer!'  # The constraint
+    if n < 10:  # Base case (stopping case)
+        return n
+    else:
+        return int(n%10) + sumOfDigits(int(n/10)) # Recursive state
+
+print("Sum of Digits:", sumOfDigits(125))
