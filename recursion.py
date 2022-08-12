bd3b8f8c44112703a75519d45ce16451271d6cec
DarinD06 <darindunlap06@gmail.com>
2022-08-12 16:29:27 -0500

    Calculating power of a number using recursion

--- recursion.py
+++ recursion.py
@@ -3,6 +3,9 @@
 
 # Using factorials
 # The recursive case for factorial is n! = n * (n-1)!
+from tkinter import N
+
+
 def factorial(n):
     assert 0 <= n == int(n), 'The number must be a positive integer!'  # The constraint
 
@@ -36,3 +39,18 @@ def sumOfDigits(n):
         return int(n%10) + sumOfDigits(int(n/10)) # Recursive state
 
 print("Sum of Digits:", sumOfDigits(125))
+
+
+# Calculating power of a number using recursion
+def powerOfNumber(base, exp):
+    assert 0 <= base == int(base), 'The base number must be a positive integer!'
+    assert 0 <= exp == int(exp), 'The exponent must be a positive integer!'  # The constraint
+
+    if exp == 0:  # Stopping Case
+        return 1
+    elif exp == 1:  # Stopping Case
+        return base
+    else:
+        return base * powerOfNumber(base, exp - 1)
+
+print("Power of a number:", powerOfNumber(3, 3))
