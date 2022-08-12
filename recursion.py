82505e89614d2a94b5bbd1d595c111533bf74a73
DarinD06 <darindunlap06@gmail.com>
2022-08-12 16:49:04 -0500

    Decimal to Binary

--- recursion.py
+++ recursion.py
@@ -3,8 +3,6 @@
 
 # Using factorials
 # The recursive case for factorial is n! = n * (n-1)!
-from tkinter import N
-
 
 def factorial(n):
     assert 0 <= n == int(n), 'The number must be a positive integer!'  # The constraint
@@ -58,9 +56,23 @@ print("Power of a number:", powerOfNumber(3, 3))
 
 # Finding greatest common divisor
 def gcd(x, y):
+    assert 0 <= x == int(x) and x > y, 'X must be a positive integer and greater than y!'  # The constraint
+    assert 0 <= y == int(y), 'Y must be a positive integer!'  # The constraint
+
     if x % y != 0:
-        return gcd(y, x%y)
+        return gcd(y, x%y)  # Flow
     else:
-        return y
+        return y  # Base case
 
 print("GCD:", gcd(456, 324))
+
+
+# Convert number from decimal to binary using recursion
+def decToBinary(n):
+    assert 0 <= n == int(n), 'n must be a positive integer!'  # The constraint
+    if n // 2 == 0:
+        return str(n % 2)  # Stop case
+    else:
+        return decToBinary(n // 2) + str(n % 2)  # Flow
+
+print("Decimal to Binary:", decToBinary(27))
